8ef36cac9bcdb848cecfadfac16d3dfab65d772d
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-08-12 15:45:20 -0400

    Created base Banner Nav

---
+++ src/components/dashboard/BannerNav.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useOktaAuth } from "@okta/okta-react/dist/OktaContext";
+import {
+  loginAction,
+  setFilter,
+  recommendationAction,
+} from "../../store/actions";
+
+// Banner img import
+import Banner from "../../img/Banner.png";
+import GroaLogo from "../../img/groa-logo-nav.png";
+
+import { Toolbar, Typography, MenuItem, CssBaseline } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+// Search Bar Component
+import SearchBar from "../auth/SearchBar.js";
+
+const useStyles = makeStyles((theme) => ({
+  logout: {
+    color: "white",
+  },
+  [theme.breakpoints.down("xs")]: {
+    groaLogo: {
+      flex: "display",
+      justifyContent: "center",
+    },
+  },
+  //for nav bar
+  toolBar: {
+    backgroundColor: "#1c1c1b",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  groaLogo: {
+    height: "2rem",
+    width: "auto",
+  },
+  Banner: {
+    width: "100%",
+  },
+}));
+
+const BannerNav = ({ userid, search }) => {
+  const theme = useTheme();
+  const classes = useStyles();
+
+  const { authState, authService } = useOktaAuth();
+
+  const logout = async () => authService.logout("/");
+
+  return (
+    <div>
+      <CssBaseline />
+      <Toolbar className={classes.toolBar}>
+        <Typography variant="h6" noWrap>
+          <img className={classes.groaLogo} src={GroaLogo} alt="Groa Logo" />
+        </Typography>
+        <MenuItem
+          onClick={logout}
+          button
+          component="a"
+          className={classes.logout}
+        >
+          Logout
+        </MenuItem>
+      </Toolbar>
+      <div>
+        <img className={classes.Banner} src={Banner} alt="Groa Banner" />
+      </div>
+
+      {search ? <SearchBar /> : <></>}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    userid: state.login.userid,
+    searchTerm: state.filter.searchTerm,
+    search: state.movie.search,
+  };
+};
+
+export default connect(mapStateToProps, {
+  loginAction,
+  recommendationAction,
+  setFilter,
+})(BannerNav);

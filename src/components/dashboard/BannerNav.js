b4372c4568f5d5b2a05098eedb28ef53cd15859f
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-08-14 00:03:13 -0400

    created mobile responsive nav

--- src/components/dashboard/BannerNav.js
+++ src/components/dashboard/BannerNav.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { connect } from "react-redux";
 import { useOktaAuth } from "@okta/okta-react/dist/OktaContext";
@@ -9,9 +9,11 @@ import {
 } from "../../store/actions";
 
 // Banner img import
-import Banner from "../../img/Banner.png";
+import Banner from "../../img/groa_banner.png";
 import GroaLogo from "../../img/groa-logo-nav.png";
 
+import { Collapse, Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Toolbar, Typography, MenuItem, CssBaseline } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -22,17 +24,14 @@ const useStyles = makeStyles((theme) => ({
   logout: {
     color: "white",
   },
-  [theme.breakpoints.down("xs")]: {
-    groaLogo: {
-      flex: "display",
-      justifyContent: "center",
-    },
-  },
   //for nav bar
   toolBar: {
-    backgroundColor: "#1c1c1b",
+    backgroundColor: "#0B0B0B",
     display: "flex",
     justifyContent: "space-between",
+    "& a:hover": {
+      color: "#07E6BC",
+    },
   },
   groaLogo: {
     height: "2rem",
@@ -40,14 +39,14 @@ const useStyles = makeStyles((theme) => ({
   },
   Banner: {
     width: "100%",
-    height: "65vh",
+    height: "50vh",
+    borderBottom: "1px solid black",
   },
   navbar: {
     display: "flex",
     flexDirection: "row",
-    padding: "1rem 0",
+    padding: ".2rem 0",
     justifyContent: "space-around",
-
     width: "100%",
     "& a": {
       fontSize: "1rem",
@@ -55,21 +54,45 @@ const useStyles = makeStyles((theme) => ({
       color: "white",
     },
     "& a:hover": {
-      color: "#07E6BC",
+      borderBottom: "1px solid #07E6BC",
     },
   },
   navs: {
-    padding: "1rem 0",
+    display: "flex",
+    flexDirection: "column",
+    padding: ".5rem 0",
     marginTop: "-5px",
-    background: "#121211",
+    background: "#020C14",
+  },
+  toggler: {
+    color: "white",
+  },
+  [theme.breakpoints.down("xs")]: {
+    groaLogo: {
+      flex: "display",
+      justifyContent: "center",
+    },
+    navs: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    navbar: {
+      flexDirection: "column",
+      margin: "1rem 0",
+    },
+    Banner: {
+      height: "20vh",
+    },
   },
 }));
 
 const BannerNav = ({ userid, search }) => {
   const classes = useStyles();
   const { authState, authService } = useOktaAuth();
-
   const logout = async () => authService.logout("/");
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   const handleScroll = (target, nav, banner) => {
     if (window.pageYOffset >= target) {
@@ -86,7 +109,6 @@ const BannerNav = ({ userid, search }) => {
     let bannerSticky = document.querySelector("#banner");
     let sticky = navSticky.offsetTop;
 
-    console.log(sticky);
     window.onscroll = function () {
       handleScroll(sticky, navSticky, bannerSticky);
     };
@@ -115,12 +137,36 @@ const BannerNav = ({ userid, search }) => {
         alt="Groa Banner"
       />
       <div id="stickyNav" className={classes.navs}>
-        <nav className={classes.navbar}>
-          <a href={`/${userid}/recommendations`}>Recommendation</a>
-          <a href={`/${userid}/watchlist`}>Watchlist</a>
-          <a href={`/${userid}/ratings`}>Ratings</a>
-          <a href={`/${userid}/explore`}>Explore</a>
-        </nav>
+        <Navbar expand="md">
+          <button
+            aria-label="Toggle navigation"
+            type="button"
+            class="navbar-toggler"
+            onClick={toggle}
+          >
+            <span class="navbar-toggler-icon">
+              <MenuIcon className={classes.toggler} />
+            </span>
+          </button>
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className={`mr-auto ${classes.navbar}`} navbar>
+              <NavItem>
+                <NavLink href={`/${userid}/recommendations`}>
+                  Recommendations
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={`/${userid}/watchlist`}>Watchlist</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={`/${userid}/ratings`}>Ratings</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={`/${userid}/explore`}>Discover</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
         {search ? <SearchBar /> : <></>}
       </div>
     </div>

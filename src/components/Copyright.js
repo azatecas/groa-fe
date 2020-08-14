f31cb040be6823935c6fe50f17303252024f1563
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-08-14 00:01:46 -0400

    created copyright component for duplicate code

---
+++ src/components/Copyright.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+
+const Copyright = () => {
+  return (
+    <Typography
+      className="footer"
+      variant="body2"
+      color="textSecondary"
+      align="center"
+    >
+      {"Copyright © "}
+      <Link color="white" to="https://github.com/Lambda-School-Labs/Groa-fe">
+        GROÁ
+      </Link>{" "}
+      2020
+      {"."}
+      <Link color="white" to="https://groa.us/privacy-policy">
+        GROÁ Privacy Policy
+      </Link>
+    </Typography>
+  );
+};
+
+export default Copyright;
